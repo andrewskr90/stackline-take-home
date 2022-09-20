9fba8ca635dc5abed0f06290a1f640088c3b332b
andrewskr90 <andrewskr90@gmail.com>
2022-09-19 17:01:26 -0700

    removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
 import { createServer } from 'miragejs'
 import { connect } from 'react-redux'
